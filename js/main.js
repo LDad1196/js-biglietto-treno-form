9f3399c37eeed3a5bc0b8d1b44f468cbe768c337
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-04-18 15:22:07 +0200

    commit per aiuto

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+
+const km = document.querySelector('#kmUser');
+console.log('SINCRONO', 'kmUser.value', kmUser.value, typeof kmUser.value);
+
+const costPerKm = 0.21;
+
+const BasePrice = km * costPerKm;
